ab2b000000b120528be71b172e2db1f2cd0fdb0e
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-06 22:14:59 +0100

    Improve mobile classes

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const svg = document.querySelector("svg");
 const links = document.querySelectorAll("a");
 const nav = document.querySelector("nav");
 const navList = document.querySelector('.navList')
+const logo = document.querySelector('.logo')
 svg.addEventListener("click", function () {
     nav.classList.add('mobileNav')
     navList.classList.add('mobileNavList')
     links.classList.add('mobileLinks')
+    logo.classList.add('mobileLogo')
 })
 
