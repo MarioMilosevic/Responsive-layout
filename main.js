2450bb354af94d7918433d038faf94388ae082bb
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-06 21:34:02 +0100

    Include li

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // "use strict";
 const svg = document.querySelector("svg");
 const links = document.querySelectorAll("a");
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
+
 svg.addEventListener("click", function () {
-    nav.classList.add('transition')
-  links.forEach((link) => {
-    link.classList.add('transition')
-  });
-});
+    nav.classList.add('mobileNav')
+})
+
