f8eaf9c7b0f8c31366bb6099599280709607c3d6
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-06 22:06:14 +0100

    Block a elements

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 const svg = document.querySelector("svg");
 const links = document.querySelectorAll("a");
 const nav = document.querySelector("nav");
-console.log(nav);
+const navList = document.querySelector('.navList')
 svg.addEventListener("click", function () {
     nav.classList.add('mobileNav')
+    navList.classList.add('mobileNavList')
+    links.classList.add('mobileLinks')
 })
 
