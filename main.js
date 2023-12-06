5e924486d16bd56c2f437ddfbe9ed49023c53f4f
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-06 21:44:08 +0100

    Mobile nav

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const svg = document.querySelector("svg");
 const links = document.querySelectorAll("a");
 const nav = document.querySelector("nav");
-
+console.log(nav);
 svg.addEventListener("click", function () {
     nav.classList.add('mobileNav')
 })
