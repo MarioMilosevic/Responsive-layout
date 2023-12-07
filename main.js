2f9cd8af0fb92087028e4e9a2b6b6dd9516cad94
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-07 11:25:34 +0100

    Add button functionality

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 // "use strict";
 const svg = document.querySelector("svg");
 const nav = document.querySelector("nav");
-const navList = document.querySelector('.navList')
-const logo = document.querySelector('.logo')
-svg.addEventListener("click", function () {
-    const x = document.createElement('button')
-    x.classList.add('xButton')
-    x.textContent = 'X'
-    nav.appendChild(x)
-    nav.classList.add('mobileNav')
-    navList.classList.add('mobileNavList')
-    logo.classList.add('mobileLogo')
-})
+const navList = document.querySelector(".navList");
+const logo = document.querySelector(".logo");
 
+svg.addEventListener("click", function () {
+  const x = document.createElement("button");
+  x.classList.add("xButton");
+  x.textContent = "X";
+  nav.appendChild(x);
+  nav.classList.add("mobileNav");
+  navList.classList.add("mobileNavList");
+  logo.classList.add("mobileLogo");
+  x.addEventListener("click", function () {
+    nav.classList.remove("mobileNav");
+    navList.classList.remove("mobileNavList");
+    logo.classList.remove("mobileLogo");
+  });
+});
