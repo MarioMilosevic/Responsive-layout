66bbaf3bf366f1f76ed0e55f24f58ea06805c7e1
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-07 11:17:51 +0100

    Add nav button

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 // "use strict";
 const svg = document.querySelector("svg");
-const links = document.querySelectorAll("a");
 const nav = document.querySelector("nav");
 const navList = document.querySelector('.navList')
 const logo = document.querySelector('.logo')
 svg.addEventListener("click", function () {
+    const x = document.createElement('button')
+    x.classList.add('xButton')
+    x.textContent = 'X'
+    nav.appendChild(x)
     nav.classList.add('mobileNav')
     navList.classList.add('mobileNavList')
-    links.classList.add('mobileLinks')
     logo.classList.add('mobileLogo')
 })
 
