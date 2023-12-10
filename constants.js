87c858a35871bf18aa5bd41918274c97cc6b9f7a
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-10 16:39:48 +0100

    Add buttons

--- constants.js
+++ constants.js
@@ -4,11 +4,12 @@ export function data() {
     const svg = document.querySelector("svg");
     const navList = document.querySelector(".navList");
     const logo = document.querySelector(".logo");
+    const rightBtn = document.querySelector('.rightBtn')
+    const leftBtn = document.querySelector('.leftBtn')
+    const sliderImgDivs = document.querySelectorAll('.sliderImg')
 
 
-
-
-    return {nav, svg, navList, logo}
+    return {nav, svg, navList, logo, leftBtn, rightBtn, sliderImgDivs}
 } 
 
 // sve ih ubacim u ovaj izmedju linija, i onda 234 poceram desno sa pozicijom apsolute, i onda overflow tu, 
--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 "use strict";
 import { data } from "./constants";
-const { nav, svg, navList, logo } = data();
+const { nav, svg, navList, logo, rightBtn, leftBtn, sliderImgDivs } = data();
 const xButton = document.querySelector(".xBtn");
-
+console.log(sliderImgDivs);
 svg.addEventListener("click", function () {
   nav.classList.add("navTranslate");
 });
 
 xButton.addEventListener("click", function () {
   nav.classList.remove("navTranslate");
+});
+
+const sliderDivTranslate = () => {
+  let currentPosition = 0;
+};
+//ako je event target isti kao sacuvani target uzmi currentPosition i dodaj ga na svaki od divova, kao transform translate vrijednost dinamicki
+let currentPosition = 0;
+
+rightBtn.addEventListener("click", function (e) {
+    currentPosition = currentPosition - 130;
+    sliderImgDivs.forEach((div) => {
+      div.style.transform = `translateX(${currentPosition}%)`;
+    });
+    console.log(currentPosition);
+});
+// });
+leftBtn.addEventListener("click", function () {
+  sliderImgDivs.forEach((sliderImg) => {
+    sliderImg.style.transform = `translateX(${130}%)`;
+  });
+  console.log("lijevp");
 });
