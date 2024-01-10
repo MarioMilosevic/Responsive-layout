5d9e3b5f206841d45ae791bf094fdf7970069f45
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-10 15:02:47 +0100

    Refactor and delete unnecesary code

--- main.js
+++ main.js
@@ -3,17 +3,10 @@ import { data } from "./constants";
 const {
   nav,
   svg,
-  navList,
-  logo,
   rightBtn,
   leftBtn,
-  sliderImgDivs,
   sliderBtns,
   xButton,
-  button0,
-  button1,
-  button2,
-  button3,
   sliderDivArr,
 } = data();
 import { moveSlideDiv } from "./helpers";
@@ -54,37 +47,15 @@ const prevSlide = () => {
 rightBtn.addEventListener("click", nextSlide);
 leftBtn.addEventListener("click", prevSlide);
 
-// KASNIJE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 sliderBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     removeClass(sliderBtns, "filledBtn");
     currentPosition = btn.id;
     if (btn.id === String(currentPosition)) {
       sliderBtns[currentPosition].classList.add("filledBtn");
-      console.log("da");
       moveSlideDiv(currentPosition, sliderDivArr)
     }
   });
 });
 
-// sliderBtns.forEach((btn) => {
-//   btn.addEventListener("click", function () {
-//     if (btn.id === "btn1") {
-//       currentPosition = 0;
-//       removeClass(sliderBtns, "filledBtn");
-//       button1.classList.add("filledBtn");
-//     } else if (btn.id === "btn2") {
-//       currentPosition = -130;
-//       removeClass(sliderBtns, "filledBtn");
-//       button2.classList.add("filledBtn");
-//     } else if (btn.id === "btn3") {
-//       currentPosition = -260;
-//       removeClass(sliderBtns, "filledBtn");
-//       button3.classList.add("filledBtn");
-//     } else if (btn.id === "btn0") {
-//       currentPosition = -390;
-//       removeClass(sliderBtns, "filledBtn");
-//       button0.classList.add("filledBtn");
-//     }
-//   });
-// });
+
